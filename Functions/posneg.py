49b47c1950995b1d43002c3dc79f4463fdf2aa09
Harishmitha <67803061+harishmitha26@users.noreply.github.com>
2022-03-28 13:46:48 +0530

    Create posneg.py

---
+++ Functions/posneg.py
@@ -0,0 +1,13 @@
+list=[44,26,28,-26,-28,5,-8]
+
+def numbers(num):
+  pos= 0
+  neg=0
+  for j in num:
+      if(j>0):
+        pos=pos+1
+      else:
+        neg=neg+1
+  print("Number of positive numbers:",pos)
+  print("Number of negative number:",neg)
+numbers(list)
